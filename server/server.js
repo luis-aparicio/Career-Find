8fff7dea6e991772e4d1d2f47fece34de167feb1
luisaparicioufl <44217585+luisaparicioufl@users.noreply.github.com>
2020-03-13 17:31:23 -0400

    add mongodb

--- server/server.js
+++ server/server.js
@@ -8,3 +8,18 @@ const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
 //app.use(passport.initialize());
 //require('./config/passport')(passport);
+
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
