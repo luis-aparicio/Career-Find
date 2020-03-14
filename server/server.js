a39472ea04d3b99f92a768ea5d19966d922d8530
mindcodemediator <46549129+mindcodemediator@users.noreply.github.com>
2020-03-13 20:57:09 -0400

    cleaned server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,7 @@
 const express = require('./config/express.js');
-const passport = require('./config/passport');
  
 // Use env port or default
 const port = process.env.PORT || 5000;
 
 const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
-//app.use(passport.initialize());
-//require('./config/passport')(passport);
-
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-  });
-    console.log("MongoDB is Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
