713881b71fa4f563827d090ad13c34396fcabb49
Josebautista1 <josebautista@ufl.edu>
2020-04-19 12:18:59 +0530

    Changes

--- client/src/views/Profile/profile.js
+++ client/src/views/Profile/profile.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from '../../assets/logo.svg';
+import './prof.css';
+import hum from './human.jpg'
 
 
-function Home() {
+function Profile() {
     return (
+        <Fragment>
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
-            </header>
+            <header className="App-header"></header>
         </div>
+        <div className="contain">
+<div className="column1">
+    <div classname="avatar">
+        <center><h1 className="heading">Avatar</h1></center>
+        <img src={hum} className="profimg" height="450" width="600">
+        </img>
+       
+    </div>
+    <button name="edit" className="editbtn">Edit Profile</button>
+</div>
+<div className="column2">
+<h3 className="username">Jose Bautista</h3>
+<h3 className="subheading">Preferred courses</h3>
+<ul className="bullet">
+    <li>Operating system</li>
+    <li>Software Engineering</li>
+    <li>Web design</li>
+</ul>
+<h3 className="subheading">About you</h3>
+<p className="desc">
+lorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem 
+Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsum
+lorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsum
+lorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsum
+lorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsumlorem Ipsum
+</p>
+</div>
+        </div></Fragment>
     );
 }
 
-export default Home;
+export default Profile;
